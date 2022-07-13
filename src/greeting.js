3985558f1883e792b39047cae58d1feb67ccdd47
nick thomas <nichoth@gmail.com>
2022-07-12 20:16:38 -0700

    tonic

---
+++ src/greeting.js
@@ -0,0 +1,11 @@
+import Tonic from '@socketsupply/tonic'
+
+class MyGreeting extends Tonic {
+    render () {
+        return this.html`<div>Hello, World.</div>`
+    }
+}
+
+Tonic.add(MyGreeting)
+
+// export default MyGreeting
