95c51529e46abd96b26a17f833d855f163182db0
nick thomas <nichoth@gmail.com>
2022-07-13 08:26:44 -0700

    add a counter

--- src/greeting.js
+++ src/greeting.js
@@ -2,8 +2,23 @@
 import Tonic from '/tonic.min.esm.js'
 
 class MyGreeting extends Tonic {
+    constructor () {
+        super()
+        this.state.counter = 0
+    }
+
+    // method name corresponds to the DOM event name
+    click (ev) {
+        ev.preventDefault()
+        this.state.counter++
+        this.reRender()
+    }
+
     render () {
-        return this.html`<div>Hello, World.</div>`
+        return this.html`
+            <div>Hello, World.</div>
+            <button>${this.state.counter.toString()}</button>
+        `
     }
 }
 
